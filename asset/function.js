67cf59d6610444b2d2a1cae716bf927b123009c9
Mehdi Sebbah <sebbah.m.dev@gmail.com>
2020-09-29 17:00:21 +0200

    commit leaflet

--- asset/function.js
+++ asset/function.js
@@ -1,3 +1,15 @@
 function myfunction(x) {
     x.classList.toggle("change");
 }
+
+const mymap = L.map('mapid',  { zoomControl: false }).setView([48.8566969,2.3514616], 40);
+const marker = L.marker([48.8566969,2.3514616]).addTo(mymap);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibWVoZGk3Nzk0IiwiYSI6ImNrZm8xdTQyeTBnOXYyc29lejczNDRrNzgifQ.HycSLIijy8Uvxw3mdlVo5w'
+}).addTo(mymap);
